175fd485ca0bafb5d4ce8b3c8f611e5e23e262f8
thewolf2114 <rcole@uccs.edu>
2020-02-03 10:06:55 -0700

    Created Prototype Level

---
+++ UntitledHalloweenGame/Assets/Scripts/Pausable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausable : MonoBehaviour
+{
+    public bool IsPaused
+    { get; set; }
+
+    virtual protected void Start()
+    {
+        IsPaused = false;
+    }
+
+    // Update is called once per frame
+    virtual protected void Update()
+    {
+        if (IsPaused)
+            return;
+
+    }
+}
