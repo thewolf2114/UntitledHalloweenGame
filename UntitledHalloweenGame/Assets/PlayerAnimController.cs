d11b625eaf537aa593285c2336aa73573efef4a8
thewolf2114 <rcole@uccs.edu>
2020-02-23 17:53:31 -0700

    Started work on animations.

---
+++ UntitledHalloweenGame/Assets/PlayerAnimController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour
+{
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForward = Input.GetAxis("Vertical");
+        float moveRight = Input.GetAxis("Horizontal");
+
+        if (moveForward > 0 || moveRight > 0)
+        {
+            playerAnimator.SetFloat("Speed_f", 1);
+        }
+        else
+        {
+            playerAnimator.SetFloat("Speed_f", 0);
+        }
+    }
+}
