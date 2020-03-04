ab39cf48a18eb056f2a1c3cf3cb9716ddf7c0c9a
Rion Cole <thewolf2114@gmail.com>
2020-03-03 19:03:13 -0700

    Created spawn location objects on all streets. Spawning houses. Houses not spawning in right locations.

--- UntitledHalloweenGame/Assets/Scripts/Managers/LevelGenerator.cs
+++ UntitledHalloweenGame/Assets/Scripts/Managers/LevelGenerator.cs
@@ -11,6 +11,9 @@ public class LevelGenerator : MonoBehaviour
     GameObject startingBoarder;
 
     Vector3 currPosition;
+    List<GameObject> roadsInGame;
+    LayerMask cornerLayer;
+    LayerMask straightLayer;
 
     const int X_INCREMENT = 60;
     const int Z_INCREMENT = 60;
@@ -24,32 +27,67 @@ public class LevelGenerator : MonoBehaviour
         Random.InitState(seed);
         Debug.Log("Seed: " + seed);
 
+        roadsInGame = new List<GameObject>();
+        cornerLayer = LayerMask.NameToLayer("Corner");
+        straightLayer = LayerMask.NameToLayer("Straight");
+
         // place the starting boarder at 0, 0, 0
         currPosition = Vector3.zero;
-        PlaceStartingBoarder(currPosition);
+        PlaceStartingBoarder();
 
         // move the current position to prepare for road generation.
         currPosition += new Vector3(X_INCREMENT / 2, 0, Z_INCREMENT / 2);
-        BuildRoads(currPosition);
+        BuildRoads();
+
+        currPosition = Vector3.zero;
+        BuildHouses();
     }
 
-    private void PlaceStartingBoarder(Vector3 position)
+    private void PlaceStartingBoarder()
     {
-        Instantiate(startingBoarder, position, Quaternion.identity);
+        Instantiate(startingBoarder, currPosition, Quaternion.identity);
     }
 
-    private void BuildRoads(Vector3 currPosition)
+    private void BuildRoads()
     {
         for (int i = 0; i < LEVEL_HEIGHT; i++)
         {
             for (int j = 0; j < LEVEL_WIDTH; j++)
             {
                 int randRoad = Random.Range(0, roads.Count);
+                GameObject newRoad = roads[randRoad];
+                roadsInGame.Add(newRoad);
 
                 Instantiate(roads[randRoad], currPosition, Quaternion.identity);
                 currPosition += new Vector3(X_INCREMENT, 0, 0);
             }
             currPosition = new Vector3(X_INCREMENT / 2, 0, currPosition.z + Z_INCREMENT);
         }
+    }
+
+    private void BuildHouses()
+    {
+        for (int i = 0; i < roadsInGame.Count; i++)
+        {
+            if (roadsInGame[i].layer == LayerMask.NameToLayer("DeadEnd"))
+                continue;
+
+            for (int j = 1; j <= 4; j++)
+            {
+                string plotName = "HousePlot_0" + j;
+                GameObject plot = roadsInGame[i].transform.Find(plotName).gameObject;
+
+                SpawnHouse(plot.transform.position, plot.layer);
+            }
+        }
+    }
+
+    private void SpawnHouse(Vector3 location, LayerMask layer)
+    {
+        layer = layer >> 14;
+        List<List<GameObject>> houses = new List<List<GameObject>>() { cornerHouses, straightHouses };
+        int randHouse = Random.Range(0, houses[layer].Count);
+
+        GameObject house = Instantiate(houses[layer][randHouse], location, Quaternion.identity) as GameObject;
     }
 }
