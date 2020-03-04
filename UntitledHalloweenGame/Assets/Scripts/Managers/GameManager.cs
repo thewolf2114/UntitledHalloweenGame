943e21bc1f5612d4d75fd4f4b1e3a480fca94ae2
Rion Cole <thewolf2114@gmail.com>
2020-03-03 17:27:11 -0700

    Starting boarder created. Started working on level generation code. Currently builds the boarder and fills the center with random roads. Need to work on adding positions for the houses. Need to work on generating the houses.

--- UntitledHalloweenGame/Assets/Scripts/Managers/GameManager.cs
+++ UntitledHalloweenGame/Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject levelGenerator;
+
+    public static GameObject Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = gameObject;
+            DontDestroyOnLoad(this);
+        }
+        else
+            Destroy(gameObject);
+
+        if (levelGenerator)
+            Instantiate(levelGenerator);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- UntitledHalloweenGame/Assets/Scripts/Managers/LevelGenerator.cs
+++ UntitledHalloweenGame/Assets/Scripts/Managers/LevelGenerator.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField]
+    List<GameObject> roads, cornerHouses, straightHouses;
+
+    [SerializeField]
+    GameObject startingBoarder;
+
+    Vector3 currPosition;
+
+    const int X_INCREMENT = 60;
+    const int Z_INCREMENT = 60;
+    const int LEVEL_WIDTH = 5;
+    const int LEVEL_HEIGHT = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int seed = Random.Range(int.MinValue, int.MaxValue);
+        Random.InitState(seed);
+        Debug.Log("Seed: " + seed);
+
+        // place the starting boarder at 0, 0, 0
+        currPosition = Vector3.zero;
+        PlaceStartingBoarder(currPosition);
+
+        // move the current position to prepare for road generation.
+        currPosition += new Vector3(X_INCREMENT / 2, 0, Z_INCREMENT / 2);
+        BuildRoads(currPosition);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlaceStartingBoarder(Vector3 position)
     {
-        
+        Instantiate(startingBoarder, position, Quaternion.identity);
+    }
+
+    private void BuildRoads(Vector3 currPosition)
+    {
+        for (int i = 0; i < LEVEL_HEIGHT; i++)
+        {
+            for (int j = 0; j < LEVEL_WIDTH; j++)
+            {
+                int randRoad = Random.Range(0, roads.Count);
+
+                Instantiate(roads[randRoad], currPosition, Quaternion.identity);
+                currPosition += new Vector3(X_INCREMENT, 0, 0);
+            }
+            currPosition = new Vector3(X_INCREMENT / 2, 0, currPosition.z + Z_INCREMENT);
+        }
     }
 }
