81d040dc5007751a779501dc66ea7efe8870abfc
Rion Cole <thewolf2114@gmail.com>
2020-03-07 11:27:10 -0700

    Rotation Test passed. Local coordinate system implemented. Local coordinate system test passed. Started working on rule set.

--- UntitledHalloweenGame/Assets/Scripts/Managers/LevelGenerator.cs
+++ UntitledHalloweenGame/Assets/Scripts/Managers/LevelGenerator.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> roads, cornerHouses, straightHouses;
+    private List<GameObject> roads, cornerHouses, straightHouses;
 
     [SerializeField]
-    GameObject startingBoarder;
+    private GameObject startingBoarder;
+
+    private enum Roads { CROSS, DEAD_END, STRAIGHT, TEE };
 
     List<GameObject> roadsInGame;
     LayerMask cornerLayer;
-    LayerMask straightLayer;
 
     const int X_INCREMENT = 60;
     const int Z_INCREMENT = 60;
-    const int LEVEL_WIDTH = 5;
-    const int LEVEL_HEIGHT = 5;
+    const int LEVEL_WIDTH = 4;  // range from 0 to 4
+    const int LEVEL_HEIGHT = 4; // range from 0 to 4
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +30,6 @@ public class LevelGenerator : MonoBehaviour
 
         roadsInGame = new List<GameObject>();
         cornerLayer = LayerMask.NameToLayer("Corner");
-        straightLayer = LayerMask.NameToLayer("Straight");
 
         // place the starting boarder at 0, 0, 0
         currPosition = Vector3.zero;
@@ -49,7 +49,7 @@ public class LevelGenerator : MonoBehaviour
     private void PlaceStartingBoarder(ref Vector3 currPosition)
     {
         Instantiate(startingBoarder, currPosition, Quaternion.identity);
-        currPosition += new Vector3(X_INCREMENT / 2, 0, Z_INCREMENT / 2);
+        currPosition = GlobalToLocal(new Vector3(X_INCREMENT / 2, 0, Z_INCREMENT / 2));
     }
 
     /// <summary>
@@ -58,22 +58,29 @@ public class LevelGenerator : MonoBehaviour
     /// <param name="currPosition">a reference to the current spot</param>
     private void BuildRoads(ref Vector3 currPosition)
     {
-        for (int i = 0; i < LEVEL_HEIGHT; i++)
-        {
-            for (int j = 0; j < LEVEL_WIDTH; j++)
-            {
-                int randRoad = Random.Range(0, roads.Count);
+        GameObject deadEnd = Instantiate(roads[1], currPosition, roads[1].transform.rotation) as GameObject;
+        deadEnd.transform.rotation *= Quaternion.Euler(0, 90, 0);
 
-                GameObject road = Instantiate(roads[randRoad], currPosition, roads[randRoad].transform.rotation) as GameObject;
-                roadsInGame.Add(road);
-                currPosition += new Vector3(X_INCREMENT, 0, 0);
-            }
-            currPosition = new Vector3(X_INCREMENT / 2, 0, currPosition.z + Z_INCREMENT);
-        }
+        //for (int i = 0; i < LEVEL_HEIGHT; i++)
+        //{
+        //    for (int j = 0; j < LEVEL_WIDTH; j++)
+        //    {
+        //        int randRoad = Random.Range(0, roads.Count);
 
-        currPosition = Vector3.zero;
+        //        GameObject road = Instantiate(roads[randRoad], LocalToGlobal(currPosition), roads[randRoad].transform.rotation) as GameObject;
+        //        roadsInGame.Add(road);
+        //        currPosition += new Vector3(1, 0, 0);
+        //    }
+        //    currPosition = new Vector3(0, 0, currPosition.z + 1);
+        //}
+
+        //currPosition = Vector3.zero;
     }
 
+    /// <summary>
+    /// Takes the list of current roads in the game and generates the 
+    /// houses for those roads. 
+    /// </summary>
     private void BuildHouses()
     {
         List<string> plotNames = new List<string>();
@@ -126,6 +133,24 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Developes a list of possible road segments that can be placed at the current position
+    /// </summary>
+    /// <param name="currPosition">The position used to spawn the next road</param>
+    /// <returns>A list of possible road segments</returns>
+    private List<GameObject> SpawnableRoads(Vector3 currPosition)
+    {
+        List<GameObject> spawnableRoads = new List<GameObject>();
+
+        return spawnableRoads;
+    }
+
+    /// <summary>
+    /// Spawns a house as a child of the given parent obect
+    /// </summary>
+    /// <param name="parent">the object parent</param>
+    /// <param name="layer">layer used to determine house type</param>
+    /// <returns></returns>
     private GameObject SpawnHouse(GameObject parent, LayerMask layer)
     {
         int houseType = (layer == cornerLayer) ? 0 : 1;
@@ -134,5 +159,33 @@ public class LevelGenerator : MonoBehaviour
 
         GameObject house = Instantiate(houses[houseType][randHouse], parent.transform) as GameObject;
         return house;
+    }
+
+    /// <summary>
+    /// Translates a local position into a global positino
+    /// </summary>
+    /// <param name="local">the position to translate</param>
+    /// <returns>The local position in global scale</returns>
+    private Vector3 LocalToGlobal(Vector3 local)
+    {
+        float newX = (local.x * 60) + 30;
+        float newZ = (local.z * 60) + 30;
+        local = new Vector3(newX, 0, newZ);
+
+        return local;
+    }
+
+    /// <summary>
+    /// Translates a Global position into a local position
+    /// </summary>
+    /// <param name="global">the position to translate</param>
+    /// <returns>The global position in local space</returns>
+    private Vector3 GlobalToLocal(Vector3 global)
+    {
+        float newX = (global.x - 30) / 60;
+        float newZ = (global.z - 30) / 60;
+        global = new Vector3(newX, 0, newZ);
+
+        return global;
     }
 }
